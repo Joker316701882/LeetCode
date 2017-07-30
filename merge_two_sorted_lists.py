539a465275bf1aecb393fcaeb474ba40f00ff9cb
GE ZHENG <dsjokerzz@gmail.com>
2017-07-31 00:14:33 +0900

    merge_two_sorted_lists

---
+++ merge_two_sorted_lists.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = result = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                result.next = l1
+                l1 = l1.next
+            else:
+                result.next = l2
+                l2 = l2.next
+            result = result.next
+        result.next = l1 or l2
+        return dummy.next
